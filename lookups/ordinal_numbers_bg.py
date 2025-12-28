0c7a2703e36ba951245d7be10aa8193445d437c5
ivan <ivankostov95@gmail.com>
2025-12-28 11:35:20 +0100

    added func to clean political party strings/ updated scraper with int_decoder

--- lookups/ordinal_numbers_bg.py
+++ lookups/ordinal_numbers_bg.py
@@ -1,3 +1,5 @@
+from editdistpy import damerau_osa
+
 ORDINAL_BG_TO_INT = {
     "ПЪРВ": 1,
     "ВТОР": 2,
@@ -27,3 +29,38 @@ ORDINAL_BG_TO_INT = {
     "ОСЕМСТОТИН": 800,
     "ДЕВЕТСТОТИН": 900
 }
+
+def cyrillic_int_decoder(word_to_decode):
+
+    #Load a dict with mapped ordinal bg
+    numbers_mapped = ORDINAL_BG_TO_INT
+    word_to_decode_upper = word_to_decode.upper()
+
+    #Threshold to max distance when comparing words
+    lex_sim_distance = 2
+
+    #Setting up an initial distance, which will be minimized
+    distance = 10
+    decoded_number = 0
+
+    for number in numbers_mapped:
+
+        levenshtein_damerau_distance = damerau_osa.distance(word_to_decode_upper, number, lex_sim_distance)
+
+        if levenshtein_damerau_distance > -1 and levenshtein_damerau_distance < distance:
+                distance = levenshtein_damerau_distance
+                decoded_number = numbers_mapped[number]
+
+    return decoded_number
+
+def string_list_decoder (assembly_name):
+
+    assembly_list = assembly_name.split(' ')
+
+    total = 0
+
+    for assembly_word in assembly_list:
+
+        total += cyrillic_int_decoder(assembly_word)
+
+    return total
